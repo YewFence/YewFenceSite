33a391c8a4d32162cd4985f67701ce3b2ed8474b
Yew Fence <cloudmapleleaf@qq.com>
2025-10-21 23:53:22 +0800

    feat:简单实现了登录功能

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
-# 1. 创建一个 Flask 应用实例
+# 一个 Flask 应用实例
 app = Flask(__name__)
-
-# 2. 定义一个路由 (Routing)
-# 告诉 Flask：如果有人访问网站的根路径 ("/")
+# 定义一个路由 (Routing)
 @app.route('/')
-def hello_world():
-    # 3. 运行这个视图函数 (View Function)
-    # 并返回 "Hello, World!"
-    return 'Hello, World!'
+def show_index_page():
+    """ 显示首页 """
+    return render_template('index.html')
+
+@app.route('/contact')
+def show_contact_page():
+    """ 显示联系页 """
+    return render_template('contact.html')
+
+@app.route('/interests')
+def show_interests_page():
+    """ 显示兴趣页 """
+    return render_template('interests.html')
 
 @app.route('/about')
-def about():
-    return 'This is the about page.'
+def show_about_page():
+    """ 显示关于页 """
+    return render_template('about.html')
+
+@app.route('/blogs')
+def show_blogs_page():
+    """ 显示博客页 """
+    return render_template('blogs_summary.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        pw = request.form.get("password", "").strip()
+        # TODO: 这里替换为你的鉴权逻辑，比如对接数据库或哈希校验
+        if pw == "ok":
+            return redirect("/management")  # 或 url_for('management')
+        else:
+            return render_template("login.html", error="密码错误"), 401
+    return render_template("login.html")
+
+@app.route('/management')
+def management():
+    """ 显示管理页 """
+    return render_template('management.html')
