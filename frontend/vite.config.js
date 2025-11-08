c88d7ea5216941fb191118295665cb4bbc5b5982
YewFence <88962785+YewFence@users.noreply.github.com>
2025-11-08 12:21:16 +0800

    refactor: 删除一个vite中的非必要开发路由

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,10 +17,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
-      },
-      '/logout': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
       }
     }
   },
